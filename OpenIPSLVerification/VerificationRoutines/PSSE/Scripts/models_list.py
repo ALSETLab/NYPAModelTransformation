3eba967a86037bbf8ba7bcfa00d58a6293d44425
marcelofcastro <decasm3@rpi.edu>
2020-02-19 11:36:38 -0500

    Adding GENCLS to be tested

--- OpenIPSLVerification/VerificationRoutines/PSSE/Scripts/models_list.py
+++ OpenIPSLVerification/VerificationRoutines/PSSE/Scripts/models_list.py
@@ -1,5 +1,5 @@
 SimType = ['Machines','Exciters','TurbineGovernors','PowerSystemStabilizers','SeriesElement','ShuntElement']
-GensExamples = ['GENSAE','GENSAL','GENROU','GENROE']
+GensExamples = ['GENCLS','GENSAL','GENSAE','GENROU','GENROE']
 EsysExamples = ['EXAC2','ESAC2A','ESST4B','URST5T','SEXS','SCRX','EXAC1','EXST1','ESST1A','IEEEX1','ESAC1A','ESDC1A','ESDC2A','IEEET2','ST5B','IEEET1','EXNI']
 TGovExamples = ['IEESGO','HYGOV','GGOV','TGOV1']
 PsstExamples = ['PSS2A','IEEEST','PSS2B']
