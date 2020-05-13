2bf88e0be1145765f202d85cc750b719878642cf
ManuelNvro <manosnavarro@gmail.com>
2020-05-13 13:22:43 -0500

    Fault Exciters Clean Up

--- OpenIPSLVerification/VerificationRoutines/OpenModelica/WorkingDir/Fault/Exciters/ESAC1A/OpenIPSL.Examples.Controls.PSSE.ES.ESAC1A_16dae.c
+++
@@ -1,9 +0,0 @@
-/* DAE residuals is empty */
- #include "OpenIPSL.Examples.Controls.PSSE.ES.ESAC1A_model.h"
-#ifdef __cplusplus
-extern "C" {
-#endif
-int OpenIPSL_Examples_Controls_PSSE_ES_ESAC1A_initializeDAEmodeData(DATA *inData, DAEMODE_DATA* daeModeData){ return -1; }
-#ifdef __cplusplus
-}
-#endif
