64298cf4a68e1fcd3b60075aaf1694af2faf290c
marcelofcastro <decasm3@rpi.edu>
2020-05-19 15:40:55 -0400

    Adding new reports: CSVGN load variation and all exciters reference step

--- OpenIPSLVerification/VerificationRoutines/OpenModelica/WorkingDir/Fault/WindTurbines/WT4G1/OpenIPSL.Examples.Wind.PSSE.WT4G.WT4G1_16dae.c
+++
@@ -1,9 +0,0 @@
-/* DAE residuals is empty */
- #include "OpenIPSL.Examples.Wind.PSSE.WT4G.WT4G1_model.h"
-#ifdef __cplusplus
-extern "C" {
-#endif
-int OpenIPSL_Examples_Wind_PSSE_WT4G_WT4G1_initializeDAEmodeData(DATA *inData, DAEMODE_DATA* daeModeData){ return -1; }
-#ifdef __cplusplus
-}
-#endif
--- OpenIPSLVerification/VerificationRoutines/OpenModelica/WorkingDir/Fault/WindTurbines/WT4G1/OpenIPSL.Examples.Wind.PSSE.WT4G.WT4G1_16dae.h
+++
@@ -1,4 +0,0 @@
-#ifndef OpenIPSL.Examples.Wind.PSSE.WT4G.WT4G1_16DAE_H
-#define OpenIPSL.Examples.Wind.PSSE.WT4G.WT4G1_16DAE_H
-#endif
-
