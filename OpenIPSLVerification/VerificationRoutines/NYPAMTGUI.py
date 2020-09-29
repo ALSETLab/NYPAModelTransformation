2abf8d7aabff805d7d67e5ca8bd4a00e83b0870c
ManuelNvro <manosnavarro@gmail.com>
2020-09-29 10:41:00 -0500

    Changed font, fixed directory / in windows

--- OpenIPSLVerification/VerificationRoutines/NYPAMTGUI.py
+++ OpenIPSLVerification/VerificationRoutines/NYPAMTGUI.py
@@ -10,11 +10,12 @@ currentDir = os.getcwd()
 root = Tk()
 
 root.title("NYPA Trasformation")
+#root.iconbitmap('figures/icon.xbm')
 
-title = Label(root, text= "NYPA Transformation and Modelica Mass Simulation Tool", font=("Arial Bold", 17))
+title = Label(root, text= "NYPA Transformation and Modelica Mass Simulation Tool", font=("ubuntu", 17))
 title.pack()
 
-created = Label(root, text= "Created by: ALSET Laboratory, 2019-2020", font=("Arial Bold", 10))
+created = Label(root, text= "Created by: ALSET Laboratory, 2019-2020", font=("ubuntu", 10))
 created.pack()
 
 rpilogo = ImageTk.PhotoImage(Image.open("figures/rpilogo.png").resize((70,70)))
@@ -25,21 +26,21 @@ nypalogo = ImageTk.PhotoImage(Image.open("figures/nypalogo.png").resize((100,80)
 lbl1 = Label(image=nypalogo)
 lbl1.pack()
 
-tool = Label(root, text= "1. Please select the modeling tool:", font=("Arial Bold", 15))
+tool = Label(root, text= "1. Please select the modeling tool:", font=("ubuntu", 15))
 tool.pack(pady = 10)
 toolselection = StringVar()
 toolselection.set("Dymola")
 tooldrop = OptionMenu(root, toolselection, "Dymola", "OpenModelica")
 tooldrop.pack()
 
-test = Label(root, text= "2. Please select the test to be performed:", font=("Arial Bold", 15))
+test = Label(root, text= "2. Please select the test to be performed:", font=("ubuntu", 15))
 test.pack(pady = 10)
 testselection = StringVar()
 testselection.set("Fault")
 testdrop = OptionMenu(root, testselection, "Fault", "LoadVariation", "ReferenceStep")
 testdrop.pack()
 
-models = Label(root, text= "3. Please select the models to simulate:", font=("Arial Bold", 15))
+models = Label(root, text= "3. Please select the models to simulate:", font=("ubuntu", 15))
 models.pack(pady = 10)
 modelselection = StringVar()
 modelselection.set("Exciters")
@@ -60,7 +61,7 @@ def show():
 	lbl4 = Label(root, text = "Simulation running, results can be found in the Working Directory and log.txt file...")
 	lbl4.pack()
 
-button = Button(root, text="Simulate!", font  =("Arial Bold", 13), command = show)
+button = Button(root, text="Simulate!", font  =("ubnutu", 13), command = show)
 button.pack(pady = 20)
 
 mainloop()
